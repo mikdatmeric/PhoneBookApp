650092bd2976bf60e77dc29293dac66b8e5f20eb
mikdatmeric <mikdat.meric61@gmail.com>
2025-04-28 04:01:23 +0300

    db migration işlemleri yapıldı
    db migration işlemleri yapıldı

--- PhoneBookApp/Src/Services/ContactService/ContactService.API/Program.cs
+++ PhoneBookApp/Src/Services/ContactService/ContactService.API/Program.cs
@@ -1,6 +1,13 @@
+using ContactService.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ContactDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
